a9f60920051a6fa9031efae982a9d67fffba5b70
Ben <bsmiller25@gmail.com>
2019-02-16 15:43:46 -0500

    view for mlb structure

--- djfb/urls.py
+++ djfb/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('mlb/season/<season_yr>/', views.mlb_season_overview, name='mso'),
 ]
--- djfb/views.py
+++ djfb/views.py
@@ -1,6 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from djfb.models import *
 
 
 def index(request):
     return HttpResponse('Hello World')
+
+
+def mlb_season_overview(request, season_yr):
+    season = MLB_Season.objects.get(year=season_yr)
+
+    context = {
+        'season': season
+    }
+
+    return render(
+        request,
+        'djfb/mlb_season_overview.html',
+        context
+    )
