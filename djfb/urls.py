f01dd161a82d3b9bc72f410129fe20ce66f9b6bb
Ben <bsmiller25@gmail.com>
2019-02-16 16:22:03 -0500

    add mlb team profile page

--- djfb/urls.py
+++ djfb/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('mlb/season/<season_yr>/', views.mlb_season_overview, name='mso'),
+    path('mlb/team/<team_id>/', views.mlb_team_profile, name='mtp'),
 ]
--- djfb/views.py
+++ djfb/views.py
@@ -19,3 +19,17 @@ def mlb_season_overview(request, season_yr):
         'djfb/mlb_season_overview.html',
         context
     )
+
+
+def mlb_team_profile(request, team_id):
+    team = MLB_Team.objects.get(id=team_id)
+
+    context = {
+        'team': team,
+    }
+
+    return render(
+        request,
+        'djfb/mlb_team_profile.html',
+        context
+    )
